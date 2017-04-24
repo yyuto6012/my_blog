30bfcb9a6f969bfce6ee96d355a0b06c7074fa41
yyuto6012 <(yyabu0618@gmail.com)>
2017-04-24 20:20:25 +0900

    add-null false restriction to timestamps

--- db/migrate/20170424111642_create_blog_categories.rb
+++ db/migrate/20170424111642_create_blog_categories.rb
@@ -4,7 +4,7 @@ class CreateBlogCategories < ActiveRecord::Migration[5.0]
       t.string :name, null: false
       t.text :description
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
